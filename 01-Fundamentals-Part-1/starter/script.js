3e4afee15fa08f81cf5fb96aaecda6d1505c92ee
Mason Wagoner <mgwagoner@gmail.com>
2023-01-30 09:38:19 -0500

    upload to mac

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -312,3 +312,35 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log(`Someone else should drive...`)
 }
 */
+
+
+// THE SWITCH STATEMENT (alternative if/else statement)
+//-------------------------------
+
+const day = 'wednesday';
+
+switch(day) {
+  case 'monday': // day === 'monday
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!')
+}
+
+const today = true
+if (today === 'wednesday' || 'thursday')
